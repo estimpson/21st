43f2292dbd3008b39cf654efa07f2ae94678ee79
Eric Stimpson <estimpson@fore-thought.com>
2018-09-22 08:04:31 -0400

    Allow 30 characters for customer part.

--- MSSQL.Red-Gate/Tables/dbo.part_customer.sql
+++ MSSQL.Red-Gate/Tables/dbo.part_customer.sql
@@ -2,7 +2,7 @@ CREATE TABLE [dbo].[part_customer]
 (
 [part] [varchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [customer] [varchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[customer_part] [varchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[customer_part] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [customer_standard_pack] [numeric] (20, 6) NOT NULL,
 [taxable] [char] (1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [customer_unit] [varchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
